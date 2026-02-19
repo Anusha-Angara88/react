4a7d24413a3241104a7dabbd8737911a3ff9a052
anushaangara <anushaangara143@gmail.com>
2026-02-19 15:18:20 +0530

    signin.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,19 @@
-import { Routes, Route } from "react-router-dom";
-import Sigin from "./Components/Sigin";
-import Login from "./Components/Login";
-import Dashboard from "./Components/Dashboard";
-import Split from "./Components/Split";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import Signin from "./Components/Signin.jsx";
+import Login from "./Components/Login.jsx";
+
+import Split from "./Components/Split.jsx";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Sigin />} />
-      <Route path="/split" element={<Split />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/dashboard" element={<Dashboard />} />
-    </Routes>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Signin />} />
+        <Route path="/split" element={<Split />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
